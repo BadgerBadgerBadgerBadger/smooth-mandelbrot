c41e2794f0b43055f3f1cafe67ba1c7ea3fd3dcf
Ophir LOJKINE <ophir.lojkine@auto-grid.com>
2019-05-31 19:47:57 +0200

    Improve performance

--- js/acolyte.js
+++ js/acolyte.js
@@ -26,7 +26,7 @@ self.onmessage = function onmessage(event) {
     }
 }
 
-async function draw() {
+function draw() {
     const data = new Uint8ClampedArray(dimensions.width * dimensions.height * 4);
 
     for (let i = 0; i < data.length; i += 4) {
@@ -41,18 +41,18 @@ async function draw() {
 
         const result = testMandelbrot(c, maxIters)
 
-        let rgb = [0, 0, 0];
         if (!result.collapses) {
             const hue = map(result.iterBeforeCollapse, 0, maxIters, 0, 1)
-            rgb = hslToRgb(hue, 1, .5);
+            const rgb = hslToRgb(hue, 1, .5);
+            data.set(rgb, i);
         }
-        data.set(rgb, i);
-        data[i + 3] = 255;
+        data[i + 3] = 255; // alpha
     }
 
     const buffer = data.buffer;
     self.postMessage({ message: 'draw', buffer }, [buffer]);
 }
+
 
 function hslToRgb(h, s, l) {
     var r, g, b;
@@ -60,15 +60,6 @@ function hslToRgb(h, s, l) {
     if (s == 0) {
         r = g = b = l; // achromatic
     } else {
-        var hue2rgb = function hue2rgb(p, q, t) {
-            if (t < 0) t += 1;
-            if (t > 1) t -= 1;
-            if (t < 1 / 6) return p + (q - p) * 6 * t;
-            if (t < 1 / 2) return q;
-            if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
-            return p;
-        }
-
         var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
         var p = 2 * l - q;
         r = hue2rgb(p, q, h + 1 / 3);
@@ -76,5 +67,14 @@ function hslToRgb(h, s, l) {
         b = hue2rgb(p, q, h - 1 / 3);
     }
 
-    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+    return [r * 255, g * 255, b * 255];
+}
+
+function hue2rgb(p, q, t) {
+    if (t < 0) t += 1;
+    if (t > 1) t -= 1;
+    if (t < 1 / 6) return p + (q - p) * 6 * t;
+    if (t < 1 / 2) return q;
+    if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+    return p;
 }
